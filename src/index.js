904bd65c237130bfed808ce3997d6cdf1862f1a9
ctellezesp <a01274912@itesm.mx>
2019-04-03 16:31:55 -0600

    Basico

--- src/index.js
+++ src/index.js
@@ -10,3 +10,52 @@ ReactDOM.render(<App />, document.getElementById('root'));
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
+
+
+class Twitter extends React.Component{
+	constructor(props){
+		super(props);
+		this.myRef = React.createRef();
+		this.subirTweet = this.subirTweet.bind(this);
+		this.state = {
+			tweets: []
+		}
+	}
+
+	subirTweet(){
+		this.setState({
+			tweets: [
+				{user: 'Carlos', date: new Date().toString(), post: this.myRef.current.value}, 
+				...this.state.tweets
+				]
+			});
+
+		this.myRef.current.value = '';
+		this.myRef.current.focus();
+	}
+
+	render(){
+		return(
+			<div id="main">
+				<input type="text" placeholder="What's Happening?" ref={this.myRef} />
+				<button onClick={this.subirTweet}>Tweet</button>
+				<div className="landing">
+					{this.state.tweets.map((tweet, index) => 
+							
+						<div key={index}>
+							<span>{tweet.user} | {tweet.date}</span><br/>
+							<span>{tweet.post}</span>
+						</div>
+						
+					)}
+					
+				</div>
+			</div>
+		);
+	}
+}
+
+ReactDOM.render(
+	<Twitter />,
+	document.getElementById('root')
+)
